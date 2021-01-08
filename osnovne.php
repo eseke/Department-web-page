15d603af58f090cfd5f3bd38d82a4891f63df2b5
Ervin <inervzg@gmail.com>
2021-01-08 20:18:50 +0100

    Add menu, osnovne, master and servisi

---
+++ osnovne.php
@@ -0,0 +1,32 @@
+<head>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class='container'>
+<?php
+    session_start();
+    header('Content-type: text/html; charset=utf-8');
+    include('header.php');
+    include('login.php');
+    include('menu.html');
+
+    include('db_conn.php');
+    $result = mysqli_query($conn,'SELECT * FROM `nastavni_plan`,`predmet` WHERE `nastavni_plan`.`sifra_predmeta`=`predmet`.`sifra_predmeta` order by `nastavni_plan`.`semestar` asc');
+    if(mysqli_num_rows($result)){
+        $hash = [];
+        while($row = mysqli_fetch_assoc($result)){
+            if($row['semestar']>8)
+                break;
+            if(!isset($hash[$row['sifra_predmeta']])){
+                $hash[$row['sifra_predmeta']] = true;
+                echo $row['sifra_predmeta']." ".$row['naziv']." ".$row['fond_casova']." ".$row['semestar']."<br/>";
+            }
+        }
+    }
+    include('db_disconn.php');
+    include('footer.html');
+    ?>
+    </div>
+</doby>
--- servisi.php
+++ servisi.php
@@ -11,6 +11,14 @@
     include('header.php');
     include('login.php');
     include('menu.html');
+?>
+<div id='servisi'>
+    <a href='http://www.etf.rs/' target="_blank">Fakultet</a><br/>
+    <a href='http://student.etf.rs/' target="_blank">Studentski servis</a><br/>
+    <a href='http://lists.etf.rs/' target="_blank">Mejling lista</a><br/>
+    <a href='http://rti.etf.rs/sale' target="_blank">Paviljoni laboratorija</a><br/>
+</div>
+<?php
     include('footer.html');
     ?>
     </div>
